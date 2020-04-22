7434afd5047c991c45a0a1fcedfb5406b7e8c974
Mauricio Oruezabal <moruezabal@gmail.com>
2020-04-22 07:39:24 -0300

    base de datos

--- db.php
+++ db.php
@@ -1,7 +1,20 @@
 <?
 
+
+function getTasks(){
+
 $db = new PDO('mysql:host=localhost;'
 .'dbname=db_tareas;charset=utf8'
-, 'root', 'root');
+, 'root', '');
+
+$sentencia = $db->prepare( "select * from tarea");
+$sentencia->execute();
+$tareas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+
+foreach($tareas as $tarea) {
+    echo $tarea->nombre;
+    }
+}
+
 
 ?>
